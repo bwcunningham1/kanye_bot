eeb390bf1302ad731d3b163f9afe1c490f993f1d
Sumedha Pramod <pramods@umich.edu>
2014-06-21 15:41:53 -0700

    updated kanye

--- HelloWorld.js
+++ HelloWorld.js
@@ -62,34 +62,6 @@ var kanye_fidence = [
 ];
 
 /************************************************************************
- * Play Book
- ***********************************************************************/
-
-var play_book = [ 
-    "Bros before hoes", 
-    "A Bro will not talk about something lame in front of a woman",
-    "Bros don't date their bro ex girlfriends",
-    "Bros buy porn for all the bros",
-    "A bro never sends a greeting card to another bro",
-    "Bros do not share dessert",
-    "Poorly-planned-mediocre-social-media-practical-jokes before honesty",
-    "A Bro shall always alert another Bro of any girl fight",
-    "Bros cannot make eye contact during a devil's threeway",
-    "A Bro will, whenever possible, provide a bro with protection",
-    "A bro saves a bro from his ex",
-    "A bro saves a bro from the friend zone",
-    "A Bro shall at all times say \"yes\"",
-    "The mom of a Bro is always off-limits. But the step-mom of a Bro is fair game if she initiates it and/or is wearing at least one article of leopards print clothing",
-    "A bro pretends to like cigars",
-    "A bro never dates a bro's ex-girlfriend (unless granted permission)",
-    "A bro that calls \"dibs\" first, has dibs",
-    "No leaving a Bro hanging",
-    "A Bro always likes the new profile picture of another bro",
-    "A true Bro will never be \"Necklace Guy\".",
-    "A bro shall not have a weird moment with another bro's fiance",
-];
-
-/************************************************************************
  * Set up the message-based IncomingStream and the HTTP push
  ***********************************************************************/
 
@@ -237,6 +209,25 @@ incoming.on('message', function(msg) {
             }
 
             /************************************************************************
+             * Kanye-fidence Compliment Generator
+             ***********************************************************************/
+            else if(txt.search("test") != -1 || txt.search("Test") != -1) {
+                message = "Test complete.";
+                API.Bots.post(
+                    ACCESS_TOKEN, // Identify the access token
+                    bot_id, // Identify the bot that is sending the message
+                    message, // Construct the message
+                    {}, // No pictures related to this post
+                    function(err,res) {
+                        if (err) {
+                            console.log("[API.Bots.post] Reply Message Error!");
+                        } else {
+                            console.log("[API.Bots.post] Reply Message Sent!");
+                        }
+                    });
+            }
+
+            /************************************************************************
              * Default BRODA responses
              ***********************************************************************/
             else if(msg["data"]["subject"]["name"] != BOT_NAME && txt.indexOf("Broda") != -1) {
@@ -268,6 +259,22 @@ incoming.on('message', function(msg) {
                     });
                   });
               });
+            }
+            else if(msg["data"]["subject"]["name"] != BOT_NAME &&
+                (txt.search("Kanye") != -1 || && txt.search("kanye") != -1)) {
+                message = "HAAAAANH?!"
+                API.Bots.post(
+                    ACCESS_TOKEN, // Identify the access token
+                    bot_id, // Identify the bot that is sending the message
+                    response.body, // Construct the message
+                    {}, // No pictures related to this post
+                    function(err,res) {
+                        if (err) {
+                            console.log("[API.Bots.post] Reply Message Error!");
+                        } else {
+                            console.log("[API.Bots.post] Reply Message Sent!");
+                        }
+                    });
             }
         }
     }
