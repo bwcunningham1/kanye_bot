f8874ada1bbb7f1c17f96c1daf052cddccd0810c
Sumedha Pramod <pramods@umich.edu>
2014-06-24 14:20:10 -0700

    added better console messages

--- HelloWorld.js
+++ HelloWorld.js
@@ -112,7 +112,7 @@ incoming.on('message', function(msg) {
 
         if (bot_id && msg["data"]["subject"]["name"] != BOT_NAME) {
             var txt = msg["data"]["subject"]["text"].toLowerCase();
-            console.dir("TXT: " + txt);
+            console.dir("[Incoming message from " + msg["data"]["subject"]["name"] + ": " + txt);
 
             /************************************************************************
              * Get current location of user
