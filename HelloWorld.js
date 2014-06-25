38c34c87ad689f6303b5ca1cee40f1aecacc40ee
Sumedha Pramod <pramods@umich.edu>
2014-06-25 14:50:59 -0700

    attempting this

--- HelloWorld.js
+++ HelloWorld.js
@@ -138,7 +138,14 @@ incoming.on('message', function(msg) {
              ***********************************************************************/
 
             //Check if last 2 messages were posted by the same person
-            API.Messages.index(ACCESS_TOKEN, function(err, response) {
+//            API.Messages.index(ACCESS_TOKEN, 8599920, function(err, response) {
+//                if(err) console.dir(err);
+//                else {
+//                    console.dir(response);
+//                    getRequest(ACCESS_TOKEN, "/groups/" + 8599920 + "/messages", {opts:opts, doParse:true}, callback);
+//                }
+//            });
+            API.getRequest(ACCESS_TOKEN, "/groups/" + 8599920 + "/messages", {opts:opts, doParse:true}, function(err, response) {
                 if(err) console.dir(err);
                 else {
                     console.dir(response);
